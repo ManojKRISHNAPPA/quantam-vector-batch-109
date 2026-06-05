430418f223009c2affd04af1e652f1f6c49d9b09
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2026-06-05 08:07:07 +0530

    updated the code

--- manoj.md
+++
@@ -1,5 +0,0 @@
-# This is from manoj branch
-# Hi Manoj
-# This is Prabhakar Chaudhari, I have commented this file
-# Thank you.
-
--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for dir in shell git jenkins docker terraform ansible kubernetes
-do
-  echo "Installing $dir"
-  mkdir $dir
-  touch $dir/README.md
-done
-
